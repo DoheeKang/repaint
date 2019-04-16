3b6f8da814f45581cf98feb66327d134398d2cf6
dohee kang <doheekang@doheeui-MacBookPro.local>
2019-04-16 15:57:46 +0900

    Drawing logic for all shape

--- src/customOverlay/Rect.js
+++ src/customOverlay/Rect.js
@@ -1,37 +1,31 @@
-import * as d3 from 'd3'
+import TwoClickShape from './TwoClickShape'
 
-class Rect extends naver.maps.OverlayView {
+class Rect extends TwoClickShape {
   constructor(options) {
     super(options)
-    this.position = options.position
   }
-  onAdd() {
-    // make svg that contain path
-    this.svg = document.createElement('svg')
-    this.svg.addEventListener('mouseover', e => {
-      console.log('정보 보여줌')
-    })
-    this._path = document.createElement('path')
-    this.svg.append(this._path)
+}
 
-    const overlayLayer = this.getPanes().overlayLayer
-    overlayLayer.appendChild(this.svg)
+Rect.prototype = Object.create(TwoClickShape.prototype)
+Rect.prototype.constructor = Rect
+
+Rect.prototype.setPath = function(spare) {
+  /* Set path */
+  this._path
+    .attr(
+      'd',
+      `M ${spare} ${spare} L ${this._widthRatio - 1} ${spare} ${this
+        ._widthRatio - 1} ${this._heightRatio - 1} ${spare} ${this
+        ._heightRatio - 1} Z`
+    )
+    .attr('stroke', this._color)
+    .attr('stroke-width', '3')
+    .attr('stroke-opacity', 1)
+  if (this._fill === 'fill') {
+    this._path.attr('fill', this._color).attr('fill-opacity', 0.6)
+  } else {
+    this._path.attr('fill', 'none')
   }
-  draw() {
-    //위도 경도를 화면상의 위치로 변환
-    const projection = this.getProjection()
-    const position = this.position
-    const pixelPosition = projection.fromCoordToOffset(position)
-    // setting svg
-    this.svg.style.position = 'absolute'
-    this.svg.style.top = `${pixelPosition.y}px`
-    this.svg.style.left = `${pixelPosition.x}px`
-    this.svg.style.width = `100px`
-    this.svg.style.height = `100px`
-    this.svg.style.backgroundColor = 'blue'
-    this.svg.style.opacity = 0.4
-  }
-  onRemove() {}
 }
 
 export default Rect
